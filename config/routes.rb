1ba75ae32b6a0e45d15c52b5d5eec73ed86d7d99
Pujith_M <pujith.m95@gmail.com>
2018-07-11 21:16:05 +0000

    Configured Devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'users/omniauth' }
+  devise_for :users, :controllers => { registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth' }
   resources :tweeets
   root "tweeets#index"
 end
