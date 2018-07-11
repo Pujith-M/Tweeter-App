1831298aaf26257011a7587143058e50aed5647b
Pujith_M <pujith.m95@gmail.com>
2018-07-11 07:17:26 +0000

    Nav bar toggle fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   def self.create_from_provider_data(provider_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
       user.email = provider_data.info.email
-      user.name = provider_data.info.email
+      user.name = provider_data.info.name
       user.username = provider_data.info.email
       user.password = Devise.friendly_token[0, 20]
       # user.skip_confirmation!
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'users/omniauth' }
   resources :tweeets
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tweeets#index"
 end
