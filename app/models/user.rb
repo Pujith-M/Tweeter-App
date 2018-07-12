fd41531b1e0bc9968ed43282f4f28ce53109cba1
Pujith_M <pujith.m95@gmail.com>
2018-07-12 06:28:39 +0000

    Removed validates_format_of :username

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          
   validates :name, presence: true
   validates :username, presence: true
-  validates_format_of :username, :with => /\A#[\w+\_]+\z/, :on => :create
+  # validates_format_of :username, :with => /\A#[\w+\_]+\z/, :on => :create
   
   has_many :tweeets, dependent: :destroy
 
@@ -15,7 +15,7 @@ class User < ApplicationRecord
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
       user.email = provider_data.info.email
       user.name = provider_data.info.name
-      user.username = provider_data.info.email.gsub(/(.+)@[^.]+.+/, '\1')
+      user.username = "#" + provider_data.info.email.gsub(/(.+)@[^.]+.+/, '\1')
       user.password = Devise.friendly_token[0, 20]
       # user.skip_confirmation!
     end
