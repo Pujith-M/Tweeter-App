f6576321e84675282f188a5b284de006ad6cbb29
Pujith M <pujith@mavenhive.in>
2019-02-14 04:06:26 +0530

    Generate unique email and username in user_factory using sequence

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,8 +1,15 @@
 FactoryBot.define do
   factory :user do
-    name {'Jhon'}
-    username {'#person'}
-    email {'someone@example.com'}
-    password {'password@123'}
+    name { 'Jhon' }
+
+    sequence :email do |n|
+      "person#{n}@example.com"
+    end
+
+    sequence :username do |n|
+      "#person#{n}"
+    end
+
+    password { 'password@123' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe User do
   describe '#follow' do
     let(:user) { create(:user) }
-    let(:friend_of_user) { create(:user, email: 'someone@example.in', username: '#user') }
+    let(:a_friend) { create(:user) }
 
     it 'should change user followers count when a friend of user follow user' do
-      expect { friend_of_user.follow(user) }.to change { user.followers.count }.by 1
+      expect { a_friend.follow(user) }.to change { user.followers.count }.by 1
     end
   end
 end
