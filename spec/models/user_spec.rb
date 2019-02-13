95a53eab5c0c775b28ce1887b8755cd52cb6568a
Pujith M <pujith@mavenhive.in>
2019-02-14 03:42:39 +0530

    Handle uniqueness of user in user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe User do
   describe '#follow' do
     let(:user) { create(:user) }
-    let(:friend_of_user) { create(:user) }
+    let(:friend_of_user) { create(:user, email: 'someone@example.in', username: '#user') }
 
     it 'should change user followers count when a friend of user follow user' do
       expect { friend_of_user.follow(user) }.to change { user.followers.count }.by 1
